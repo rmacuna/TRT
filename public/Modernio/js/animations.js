3c5405902ad764777a501c669b20d075da1903f4
Roberto Acuña <robertoacunapolo@gmail.com>
2019-12-08 15:54:15 -0500

    Type: Enchancement
    First part of the website made it

---
+++ public/Modernio/js/animations.js
@@ -0,0 +1,12 @@
+const background_image = document.getElementById("animBackground");
+
+// setTimeout(() => {
+//   background_image.classList.remove("modernio--background-1");
+//   background_image.classList.add("modernio--background-2");
+// }, 1000);
+
+// anime({
+//   targets: ".background--container-furnitureHd",
+//   delay: 400,
+//   backgroundImage: "../assets/images/modern_minimal_design.jpg"
+// });
